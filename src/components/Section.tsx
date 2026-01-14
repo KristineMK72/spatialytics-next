0274d8e997463fe0b83c53e96de6cc0f1d63456e
Kristine Kahler <124746855+KristineMK72@users.noreply.github.com>
2026-01-14 16:53:09 -0600

    Update Section.tsx

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -5,5 +5,9 @@ export default function Section({
   children: React.ReactNode;
   className?: string;
 }) {
-  return <section className={`mt-14 ${className}`}>{children}</section>;
+  return (
+    <section className={`mt-10 md:mt-14 lg:mt-16 ${className}`}>
+      {children}
+    </section>
+  );
 }
